6b6222cf398150b9d05e4cc0391ada3073b8e5ce
mariagarciaflores <grissgarcia74@gmail.com>
2018-12-06 15:17:11 -0400

    upload images

--- src/app/users/clients/client-profile/client-profile.component.ts
+++ src/app/users/clients/client-profile/client-profile.component.ts
@@ -7,6 +7,7 @@ import { first } from 'rxjs/operators';
 
 import { UsersService } from '../../users.service';
 import { PetService } from 'src/app/pets/pet.service';
+import { AuthenticationService } from 'src/app/authentication/authentication.service';
 
 @Component({
   selector: 'vet-client-profile',
@@ -18,24 +19,32 @@ export class ClientProfileComponent implements OnInit {
   pets$: Observable<any>;
   clientId: string;
   newPet: any;
-  currentPicture: File;
+  currentPetPicture: File;
+  currentClientPicture: File;
+  loggedUserIsAdmin: boolean;
 
   private pictureFileReader: FileReader;
   @ViewChild('petImage') private petImage: ElementRef;
+  @ViewChild('clientImage') private clientImage: ElementRef;
 
   constructor(
     private userService: UsersService,
     private petService: PetService,
+    private auth: AuthenticationService,
     private router: Router,
     private route: ActivatedRoute
   ) {
-    this.client = {};
+    this.client = {
+      photo: 'assets/img/no-photo2.png'
+    };
     this.newPet = {};
+    this.loggedUserIsAdmin = false;
 
     this.pictureFileReader = new FileReader();
 
     this.pictureFileReader.onloadend = () => {
       this.petImage.nativeElement.src = this.pictureFileReader.result;
+      this.clientImage.nativeElement.src = this.pictureFileReader.result;
     };
   }
 
@@ -47,10 +56,15 @@ export class ClientProfileComponent implements OnInit {
         .pipe(first())
         .subscribe(client => {
           this.client = client;
+          if (!client.photo) {
+            this.client.photo = 'assets/img/no-photo2.png';
+          }
           this.setNewPet();
         });
-      console.log(this.clientId);
       this.pets$ = this.petService.getPetsByClient(this.clientId);
+      this.auth.getLoggedUser().pipe(first()).subscribe(user => {
+        this.loggedUserIsAdmin = user.isAdmin;
+      });
     });
   }
 
@@ -60,8 +74,8 @@ export class ClientProfileComponent implements OnInit {
       .createPet(this.newPet)
       .pipe(first())
       .subscribe(pet => {
-        if (this.currentPicture) {
-          this.petService.savePetPicture(this.currentPicture, pet);
+        if (this.currentPetPicture) {
+          this.petService.savePetPicture(this.currentPetPicture, pet);
         }
       });
     this.setNewPet();
@@ -71,9 +85,9 @@ export class ClientProfileComponent implements OnInit {
     this.newPet = {};
     this.newPet.ownerId = this.clientId;
     this.newPet.ownerName = this.client.name;
-    this.newPet.photo = 'assets/img/pet-user.png';
-    this.currentPicture = null;
-    this.petImage.nativeElement.src = 'assets/img/no-photo.png';
+    this.newPet.photo = 'assets/img/pet-photo.png';
+    this.currentPetPicture = null;
+    this.petImage.nativeElement.src = 'assets/img/pet-photo.png';
   }
 
   checkPetClinicalRecords(): void {
@@ -83,11 +97,24 @@ export class ClientProfileComponent implements OnInit {
   setPetPicture(event) {
     let picture = event.target.files[0];
     if (picture) {
-      this.currentPicture = picture;
+      this.currentPetPicture = picture;
       this.pictureFileReader.readAsDataURL(picture);
     } else {
-      this.currentPicture = null;
+      this.currentPetPicture = null;
       this.petImage.nativeElement.src = 'assets/img/no-photo.png';
     }
+  }
+
+  saveClientPicture(event) {
+    let picture = event.target.files[0];
+    if (picture) {
+      this.currentClientPicture = picture;
+      this.pictureFileReader.readAsDataURL(picture);
+      this.userService.saveClientPicture(this.currentClientPicture, this.client);
+    } else {
+      this.currentClientPicture = null;
+      this.clientImage.nativeElement.src = 'assets/img/no-photo2.png';
+    }
+
   }
 }
--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { from, Observable } from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
+import { switchMap, finalize, first } from 'rxjs/operators';
 import { PetService } from '../pets/pet.service';
+import { AngularFireStorage } from '@angular/fire/storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
-  constructor(private pet: PetService, private fs: AngularFirestore) {}
+  constructor(
+    private pet: PetService,
+    private fs: AngularFirestore,
+    private storage: AngularFireStorage
+  ) {}
 
   hardUpdateUser(oldId, uid, user): Observable<void> {
     user.id = uid;
@@ -33,10 +38,14 @@ export class UsersService {
 
   registerUser(user) {
     user.id = this.fs.createId();
+    return this.setClient(user);
+  }
+
+  setClient(user) {
     return this.fs
-      .collection('users')
-      .doc(user.id)
-      .set(user, { merge: true });
+    .collection('users')
+    .doc(user.id)
+    .set(user, { merge: true });
   }
 
   getClients(): Observable<any[]> {
@@ -57,7 +66,7 @@ export class UsersService {
       .valueChanges();
   }
 
-  getClientById(id: string) {
+  getClientById(id: string): Observable<any> {
     return this.fs
       .collection('users', ref => ref.where('isAdmin', '==', false))
       .doc(id)
@@ -66,8 +75,28 @@ export class UsersService {
 
   getUserById(id: string) {
     return this.fs
-    .collection('users')
-    .doc(id)
-    .valueChanges();
+      .collection('users')
+      .doc(id)
+      .valueChanges();
+  }
+
+  saveClientPicture(picture: File, client) {
+    const filePath = `clients/${client.id}.jpg`;
+    const fileRef = this.storage.ref(filePath);
+    const task = this.storage.upload(filePath, picture);
+    task
+      .snapshotChanges()
+      .pipe(
+        finalize(() => {
+          fileRef
+            .getDownloadURL()
+            .pipe(first())
+            .subscribe(URL => {
+              client.photo = URL;
+              this.setClient(client);
+            });
+        })
+      )
+      .subscribe();
   }
 }
